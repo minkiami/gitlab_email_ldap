b2ad2c79ddc69db92a17c015df668640ba6381e2
minkiami <43140467+minkiami@users.noreply.github.com>
2018-12-20 12:50:45 +0100

    Update patch_ldap.sh

--- patch_ldap.sh
+++ patch_ldap.sh
@@ -1,9 +1,11 @@
 #! /bin/sh
 
-id=$1
-email=$2
-
-gitlab-psql -c "insert into emails (user_id,email,created_at,updated_at,confirmed_at) values ($id,'$email',now(),now(),now());"
-gitlab-psql -c "update users set email='$email' where id=$id;"
+username=$1
 
 
+utente=$(gitlab-psql -c "select id,username from users where username= '$username';" |grep $username | cut -c2-3)
+
+email=$username@fake.org
+
+gitlab-psql -c "insert into emails (user_id,email,created_at,updated_at,confirmed_at) values ($utente,'$email',now(),now(),now());"
+gitlab-psql -c "update users set email='$email' where id=$utente;"
